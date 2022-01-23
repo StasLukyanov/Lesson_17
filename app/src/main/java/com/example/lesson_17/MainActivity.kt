b8a17ff1d090676fc2ca5f80093717b307f220b6
StasLukyanov <lstas85@gmail.com>
2022-01-23 19:19:13 +0300

    таймер

--- app/src/main/java/com/example/lesson_17/MainActivity.kt
+++ app/src/main/java/com/example/lesson_17/MainActivity.kt
@@ -2,10 +2,68 @@ package com.example.lesson_17
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
+import android.view.View
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
+import android.widget.Toast
+import com.example.lesson_17.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var timer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.apply {
+            btnStart.setOnClickListener {
+                startCountDownTimer((edTimer.text.toString().toLong()+1) * 1000)
+                binding.tvTimer.text = ""
+                binding.checkBox.isChecked = false
+                binding.checkBox.visibility = View.GONE
+                binding.btnReset.text = "Reset"
+            }
+        }
+
+        binding.btnPause.setOnClickListener {
+            timer?.cancel()
+
+        }
+
+        binding.btnReset.setOnClickListener {
+            binding.checkBox.visibility = View.VISIBLE
+            binding.tvTimer.text = "Вы уверены, что хотите сбросить таймер?"
+            binding.btnReset.text = "Ok"
+            if (binding.checkBox.isChecked) {
+                timer?.cancel()
+                binding.edTimer.setText("0")
+                binding.tvTimer.text = "Finish"
+                binding.btnReset.text = "Reset"
+                binding.checkBox.isChecked = false
+                binding.checkBox.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun startCountDownTimer(timeMillis: Long) {
+        timer?.cancel()
+        timer = object : CountDownTimer(timeMillis, 1) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.edTimer.setText((millisUntilFinished / 1000).toString())
+
+            }
+
+            override fun onFinish() {
+                binding.tvTimer.text = "Finish"
+            }
+
+        }.start()
     }
 }
---
+++ app/src/test/java/com/example/lesson_17/onClickTimer.kt
@@ -0,0 +1,5 @@
+package com.example.lesson_17
+
+class onClickTimer {
+
+}
